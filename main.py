0fbf4106cdc255bf53f59b10be1b60281b5c30ed
magalaReuben <magalareuben60@gmail.com>
2024-05-04 17:41:32 +0300

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import streamlit as st
+from tornado.web import RequestHandler
+from injectApi import CustomRule, init_global_tornado_hook, uninitialize_global_tornado_hook
+
+class HelloWorldHandler(RequestHandler):
+    def get(self):
+        self.write({
+            "text": "Hello World"
+        })
+        
+    def post(self):
+        self.set_status(200)
+        self.write({
+            "text": "Hello World"
+        })
+        
+    def check_xsrf_cookie(self) -> None:
+        return None
+
+init_global_tornado_hook([CustomRule("/hello", HelloWorldHandler)])
 
 if not hasattr(st, 'already_started_server'):
     # Hack the fact that Python modules (like st) only load once to
@@ -12,27 +31,6 @@ if not hasattr(st, 'already_started_server'):
         Just close this browser tab and open a new one to see your Streamlit
         app.
     ''')
-
-    from flask import Flask
-    from flask import request
-
-    app = Flask(__name__)
-
-    @app.route('/foo')
-    def serve_foo():
-        return 'This page is served via Flask!'
-    
-    @app.route('/bar')
-    def serve_bar():
-        return 'This page is served via Flask!'
-    
-    @app.route('/', methods=['POST', 'GET'])
-    def receive_data():
-        data = request.get_json()
-        print(data)
-        return 'Data received successfully!'
-
-    app.run(port=8888)
 
 
 # We'll never reach this part of the code the first time this file executes!
